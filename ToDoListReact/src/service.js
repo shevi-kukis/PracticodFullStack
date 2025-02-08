e16d29259f156f847f5b8b6d30377ac37b177de1
shevi <shevi3151@gmail.com>
2025-02-08 23:28:19 +0200

    my commit

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -19,31 +19,25 @@ export default {
   },
 
   addTask: async (name) => {
-    try {
+ 
       const newTask = { name, isComplete: false };
       const result = await axios.post("/items", newTask);
       return result.data;
-    } catch (error) {
-      return null;
-    }
+  
   },
 
   setCompleted: async (id, isComplete, name) => {
-    try {
+  
       const updatedTask = { id, isComplete, name };
       await axios.put(`/items/${id}`, updatedTask);
       return true;
-    } catch (error) {
-      return false;
-    }
+   
   },
 
   deleteTask: async (id) => {
-    try {
+
       await axios.delete(`/items/${id}`);
       return true;
-    } catch (error) {
-      return false;
-    }
+    
   }
 };
