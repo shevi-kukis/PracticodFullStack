949c2b3a807b10756bdde26e0867d6f2d25da3ba
shevi <shevi3151@gmail.com>
2025-02-09 17:58:20 +0200

    trying

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -14,18 +14,20 @@ builder.Services.AddCors(options =>
     });
 });
 
-// קבלת ה-Connection String ממשתנה סביבה בלבד
-var connectionString = Environment.GetEnvironmentVariable("ToDoDB");
 
-Console.WriteLine($"🔍 Connection String: {connectionString}");
+// var connectionString = Environment.GetEnvironmentVariable("ToDoDB");
 
-
+// Console.WriteLine($"🔍 Connection String: {connectionString}");
 builder.Services.AddDbContext<ToDoDbContext>(options =>
-    options.UseMySql(
-        connectionString,
-        ServerVersion.AutoDetect(connectionString),
-        mysqlOptions => mysqlOptions.EnableRetryOnFailure()
-    ));
+options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"),
+new MySqlServerVersion(new Version(8, 0, 0))));
+
+// builder.Services.AddDbContext<ToDoDbContext>(options =>
+//     options.UseMySql(
+//         connectionString,
+//         ServerVersion.AutoDetect(connectionString),
+//         mysqlOptions => mysqlOptions.EnableRetryOnFailure()
+//     ));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
