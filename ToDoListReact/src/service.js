6200b049b66d1c44a49351ac445420ffbd3f0972
shevi <shevi3151@gmail.com>
2025-02-11 01:25:27 +0200

    good luck

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const apiUrl = process.env.REACT_APP_API_URL;; 
-const apiUrl=`https://${process.env.REACT_APP_API_DOMAIN}`
+const apiUrl = process.env.REACT_APP_API_URL;
+
 axios.defaults.baseURL = apiUrl;
 
 
